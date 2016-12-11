abc5797639fecfa9744acca406e0abf618f931de
Andrew Owen <yumaikas94@gmail.com>
2016-12-11 04:53:20 -0500

    We now have a moving skeleton

--- enemies.lua
+++ enemies.lua
@@ -10,7 +10,7 @@ end
 local function next_path(self)
 	-- Current x and y are set up already
 	self.path.selectedIdx = self.path.selectedIdx + 1
-	if self.path.selectedIdx < #self.path.selectedIdx then
+	if self.path.selectedIdx > #self.path then
 		self.path.selectedIdx = 1
 	end
 	local dest_pair = self.path:current()
@@ -19,9 +19,11 @@ local function next_path(self)
 	local dest = {}
 	dest[dimension] = dest_pair[dimension]
 	self.tween = flux.to(self, dest_pair.time, dest)
+	self.tween:ease("quartinout")
 	self.tween:oncomplete(function()
 		self:switch_xy()
 	end)
+	print (self.tween)
 end
 
 local function switch_xy(self)
@@ -35,18 +37,19 @@ local function switch_xy(self)
 	local dest = {}
 	dest[dimension] = dest_pair[dimension]
 	self.tween = flux.to(self, dest_pair.time, dest)
+	self.tween:ease("quartinout")
 	self.tween:oncomplete(function()
 		self:next_path()
 	end)
 end
 
 local function update(self, dt)
+	flux.update(dt)
 	-- So we have 3 states to consider: Moving on x, monving on y, and when 
-	if tween then
-		tween:update(dt)
-	else 
-		self:next_path()
-	end
+	--if not self.tween then
+	--else 
+		--self:next_path()
+	-- end
 
 end
 
@@ -61,7 +64,7 @@ function get_random_path(start_point, bounds)
 	for q = 1, 1 + math.random(10), 1 do 
 		local _x = xBounds.min + math.random(xBounds.max - xBounds.min)
 		local _y = yBounds.min + math.random(yBounds.max - yBounds.min)
-		local _time = 0.0075 * math.dist(_x, _y, startx, starty)
+		local _time = 0.00075 * math.dist(_x, _y, startx, starty)
 		table.insert(path,{x = _x , y = _y, time = _time})
 		startx = _x
 		starty = _y
@@ -72,26 +75,32 @@ function get_random_path(start_point, bounds)
 	return path
 end
 
-function spawn_skelton(x, y, dir)
+function spawn_skeleton(x, y, dir)
 
-	return {
+	local skel = {
 		-- Renderable
 		x = x,
 		y = y,
-		image = images["skelton.png"],
+		image = images["skeleton.png"],
 		scale = 2.0,
 		rotation = dirToAngle(dir),
 
 		-- Collideable
 		collides = collides,
+		-- Update
+		update = update,
 		-- Skeleton AI info
-		path = get_random_path(x, y, getTableBounds()),
+		path = get_random_path({x = x, y = y} , getTableBounds()),
 		traveling_on = "",
 		travel_timer = 0,
 		prev_x  = x,
 		prev_y = y,
 		-- Used for the tweening function to call per frame
-		tween = nil,
-
+		next_path = next_path,
+		switch_xy = switch_xy,
+		rm_update = false,
+		rm_render = false,
 	}
+	skel:next_path()
+	return skel
 end
