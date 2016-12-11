36a4f9b8d37520b0c2a7a7644da6102119fc6baa
Andrew Owen <yumaikas94@gmail.com>
2016-12-11 03:47:44 -0500

    More WIP enemy stuff

--- enemies.lua
+++ enemies.lua
@@ -7,7 +7,10 @@ local function collides(self, spells)
 end
 
 local function update(self, dt)
+	local dest = self.path:current()
 
+	local dimension = self.traveling_on
+	local self[dimension] = math.lerp()
 
 end
 
@@ -16,18 +19,21 @@ function get_random_path(start_point, bounds)
 	startx, starty = start_point.x, start_point.y
 	xBounds, yBounds = bounds.x, bounds.y
 	-- body
-	ret = {
+	path = {
 		selectedIdx = 1
 	}
 	for q = 1, 1 + math.random(10), 1 do 
 		local _x = xBounds.min + math.random(xBounds.max - xBounds.min)
 		local _y = yBounds.min + math.random(yBounds.max - yBounds.min)
 		local _time = 0.0075 * math.dist(_x, _y, startx, starty)
-		table.insert(ret,{x = _x , y = _y})
+		table.insert(path,{x = _x , y = _y, time = _time})
 		startx = _x
 		starty = _y
 	end
-	return ret
+	path.current = function (self)
+		return self[self.selectedIdx]
+	end
+	return path
 end
 
 function spawn_skelton(x, y, dir)
@@ -40,10 +46,13 @@ function spawn_skelton(x, y, dir)
 		scale = 2.0,
 		rotation = dirToAngle(dir),
 
-
 		-- Collideable
 		collides = collides,
-		-- Skeleton info
-		path = get_random_path()
+		-- Skeleton AI info
+		path = get_random_path(x, y, getTableBounds()),
+		traveling_on = "x",
+		travel_timer = 0,
+		prev_x  = x,
+		prev_y = y
 	}
 end
