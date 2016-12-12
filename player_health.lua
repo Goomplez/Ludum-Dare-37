d79fdd5080a250bb6908e28eb7b12646aee0f9ad
Andrew Owen <yumaikas94@gmail.com>
2016-12-12 15:07:23 -0500

    Clean up player_health.lua file to help get John something to work off of.

--- player_health.lua
+++ player_health.lua
@@ -1,6 +1,10 @@
 local player = require("player")
 local images = require("images")
 
+-- This brings in getTableBounds()
+require("tablebackground")
+
+-- Because this is local, it won't be visible outside this file
 local hp_images = {
 	[0] = images["hp0.png"],
 	images["hp25.png"],
@@ -8,18 +12,14 @@ local hp_images = {
 	images["hp75.png"],
 	images["hp100.png"],
 }
--- 
-local function update(self, dt)
-	self.image = hp_images[player.HP]
-end
 
+-- getTableBounds is in global scope becaue tablebackground was require
 local bounds = getTableBounds()
 
-x, y = 0, bounds.y.max + 32 * 3
-
 local HP_bar = {
-	x = x,
-	y = y,
+	-- Start of things this table needs to be rendered
+	x = 0,  
+	y = bounds.y.max + 32 * 3,
 	scale = {
 		x = 2.0,
 		y = 2.0
@@ -28,11 +28,20 @@ local HP_bar = {
 	rotation = 0,
 	image = hp_images[player.HP],
 
-	update = update,
-	rm_update = false,
+	-- If this needs to be removed from the list of rendered things, HP_bar.update should set rm_render to true
 	rm_render = false,
+	-- End of things the object needs to be rendered
+
+	-- The flag for if this should be removed from the list of updatable things
+	rm_update = false,
 }
 
+-- This adds the update function to the HP_bar table. The update function needs to be there for it to be updateable.
+function HP_bar.update(self, dt)
+	self.image = hp_images[player.HP]
+end
+
+-- Becaue we didn't make this function local, anywhere file that does require("player_health") will be able to see it.
 function load_hp_bar()
 	addRenderable(HP_bar)
 	addUpdateable(HP_bar)
