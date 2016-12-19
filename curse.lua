beaee52f4e6a3d8d516a678030d7c3067028a6ac
Andrew Owen <yumaikas94@gmail.com>
2016-12-19 01:48:24 -0500

    WIP adding derpball

---
+++ curse.lua
@@ -0,0 +1,44 @@
+-- Show the cursed stuff here
+local font = require("fonts")
+
+local curse = {
+  x = 0,
+  y = 0,
+  blink_time = .50,
+  blink_timer = 0,
+  enabled_time = 5,
+  enabled_timer = 0,
+  rm_update = false,
+  rm_render = false,
+}
+
+function curse.enable(self)
+  self.enabled_timer = 0
+end
+
+function curse.enabled(self)
+  return self.enabled_timer < self.enabled_time
+end
+
+function curse.update(self, dt)
+  if not self:enabled() then
+    return
+  end
+  self.enabled_timer = self.enabled_timer + dt
+  
+  self.blink_timer = self.blink_timer + dt
+  if self.blink_timer > self.blink_time then
+    self.blink_timer = 0
+  end
+end
+
+function curse.render(self, dt)
+  if not self.enabled then
+    return
+  end
+  if self.blink_timer > (self.blink_time / 2) then
+    font.print(self.x, self.y, "CURSED!!")
+  end
+end
+
+return curse
---
+++ derpball.lua
@@ -0,0 +1,57 @@
+-- Derpball
+local curse = require("curse")
+local images = require("images")
+local sounds = require("sounds")
+local player = require("player")
+require ("utils")
+
+local function update(self, dt)
+	local angle = math.angle(self.x, self.y, player.x, player.y)
+	self.x, self.y = offsetByVector({x = self.x, y = self.y}, angle, self.velocity * dt)
+  self.rotation = angle
+
+  self.life_timer = self.life_timer + dt
+  if self.life_timer > self.life_time then
+    self.rm_render = true
+    self.rm_update = true
+    self.rm_other_spell = true
+  end
+end
+
+local function collide(self)
+  self.rm_render = true
+  self.rm_update = true
+  self.rm_other_spell = true
+  curse:enable()
+end
+
+function spawn_derpball(x,y)
+	return {
+		-- Renderable
+		x = x,
+		y = y,
+		r = 10,
+		image = images["derpball.png"],
+		rotation = dirToAngle(direction),
+		scale = { x =2.0, y = 2.0, },
+		offset = {
+			x = 5,
+			y = 4,
+		},
+
+    life_timer = 0,
+    life_time = 5,
+
+
+		-- Updateable
+		velocity = 300,
+		update = update,
+    -- Other spell
+    collide = nil,
+
+		-- Removal flags
+		rm_render = false,
+		rm_update = false,
+		rm_other_spell = false,
+	}
+end
