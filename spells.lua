1b1851b4da647078f69218630aaecdbb27dd922c
Andrew Owen <yumaikas94@gmail.com>
2016-12-19 22:10:14 -0500

    Switched to lighting

--- spells.lua
+++ spells.lua
@@ -4,7 +4,6 @@ local player = require("player")
 local derpball = require("derpball")
 local function update(self, dt)
 	self.x, self.y = offsetByVector(self, self.rotation + (math.pi), dt * self.velocity)
-
 	if self.x < -10 or self.y < -10 or self.y > g_height or self.x > g_height then
 		self.rm_render = true
 		self.rm_update = true
@@ -52,6 +51,23 @@ function spawn_fireball(x, y, direction)
 	}
 end
 
+local function update_shot(self, dt)
+	self.x, self.y = offsetByVector(self, self.rotation + (math.pi / 2), dt * self.velocity)
+	if self.x < -10 or self.y < -10 or self.y > g_height or self.x > g_height then
+		self.rm_render = true
+		self.rm_update = true
+		self.rm_player_spell = true
+		self.rm_enemy = true
+	end
+	self.toggle_timer = self.toggle_timer + dt
+	if self.toggle_timer > self.toggle_time then
+		self.toggle_timer = 0
+		self.image = images["lightning1.png"]
+	elseif self.toggle_timer > (self.toggle_time / 2) then
+		self.image = images["lightning2.png"]
+	end
+end
+
 function spawn_spell(x, y, direction)
 	-- Renderable, updateable, colliable
 	return {
@@ -59,8 +75,8 @@ function spawn_spell(x, y, direction)
 		x = x,
 		y = y,
 		r = 5,
-		image = images["iceball.png"],
-		rotation = dirToAngle(direction) - (math.pi * 3 / 64) + ( math.random(5) * math.pi / 64),
+		image = images["lightning1.png"],
+		rotation = dirToAngle(direction) + (math.pi / 2) - (math.pi * 3 / 64) + ( math.random(5) * math.pi / 64),
 		scale = { x =2.0, y = 2.0, },
 		offset = {
 			x = 5,
@@ -69,7 +85,10 @@ function spawn_spell(x, y, direction)
 
 		-- Updateable
 		velocity = 300,
-		update = update,
+		update = update_shot,
+
+		toggle_time = .1,
+		toggle_timer = 0,
 
 		-- Removal flags
 		rm_render = false,
